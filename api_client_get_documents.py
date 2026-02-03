d4a44eaec942933908395e01e9bb3788fd663fc8
Никита <nvikulin@vepay.online>
2026-02-03 17:34:06 +0400

    8.2 Практикуемся в применении Pydantic в API клиентах: документы - Внесены правки в скрипт

--- api_client_get_documents.py
+++ api_client_get_documents.py
@@ -1,30 +1,32 @@
-import logger
+import logging
 
 from clients.http.gateway.accounts.client import build_accounts_gateway_http_client
 from clients.http.gateway.documents.client import build_documents_gateway_http_client
 from clients.http.gateway.users.client import build_users_gateway_http_client
 
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+logger = logging.getLogger(__name__)
 
 users_client = build_users_gateway_http_client()
 accounts_client = build_accounts_gateway_http_client()
 documents_client = build_documents_gateway_http_client()
 
-    # 1) Create user
+# 1) Create user
 create_user_response = users_client.create_user()
-print("Create user response: %s", create_user_response)
+logger.info("Create user response: %s", create_user_response.model_dump(by_alias=True))
 
-user_id = create_user_response["user"]["id"]
+user_id = create_user_response.user.id
 
-    # 2) Open credit card account
+# 2) Open credit card account
 open_credit_card_account_response = accounts_client.open_credit_card_account(user_id)
-print("Open credit card account response: %s", open_credit_card_account_response)
+logger.info("Open credit card account response: %s", open_credit_card_account_response)
 
 account_id = open_credit_card_account_response["account"]["id"]
 
-    # 3) Get tariff document
+# 3) Get tariff document
 tariff_doc = documents_client.get_tariff_document(account_id)
 logger.info("Get tariff document response: %s", tariff_doc.model_dump(by_alias=True))
 
-    # 4) Get contract document
+# 4) Get contract document
 contract_doc = documents_client.get_contract_document(account_id)
 logger.info("Get contract document response: %s", contract_doc.model_dump(by_alias=True))
